9f0490577822034fc20618f15e4c0aef6f9d4748
Guy Klainer <guyklainer@gmail.com>
2021-07-06 21:58:31 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,40 @@ const token =
 export const run = async () => {
   if (!token) throw new Error("GitHub token not found");
   const octokit = getOctokit(token);
+  
+  if (!context.payload.pull_request)
+    return console.log("No pull request found");
 
-  const ms: string = getInput("milliseconds");
-  debug(`Waiting ${ms} milliseconds ...`);
+  console.log(JSON.stringify(pull_request))
+  
+//   const pullRequest = (context as any).payload
+//     .pull_request as EventPayloads.WebhookPayloadPullRequestPullRequest;
 
-  debug(new Date().toTimeString());
-  await wait(parseInt(ms, 10));
-  debug(new Date().toTimeString());
-
-  setOutput("time", new Date().toTimeString());
+//   const reviews = await octokit.pulls.listReviews({
+//     owner: context.repo.owner,
+//     repo: context.repo.repo,
+//     pull_number: pullRequest.number,
+//   });
+//   const allApproved = reviews.data.every(
+//     (review) => review.state === "APPROVED"
+//   );
+//   if (allApproved) {
+//     if (reviews.data.length >= Number(getInput("approvals") || 1)) {
+//       await octokit.issues.addLabels({
+//         owner: context.repo.owner,
+//         repo: context.repo.repo,
+//         issue_number: pullRequest.number,
+//         labels: (getInput("labels") || "")
+//           .split(",")
+//           .map((label) => label.trim()),
+//       });
+//       console.log("Added labels");
+//     } else {
+//       console.log("Number of approvals not met");
+//     }
+//   } else {
+//     console.log("All reviews are not approved");
+//   }
 };
 
 export const wait = (milliseconds: number) => {
